c614123f78f7ae12fa4851c1e1647ae2e24be01f
HSaad <hagrsaad16@gmail.com>
2018-03-14 11:21:50 -0400

    build tree method

--- binary_tree.rb
+++ binary_tree.rb
@@ -7,17 +7,62 @@ class Node
 		@left_child = left_child
 		@right_child = right_child
 	end
+
+	def to_s
+		return @value
+	end
 end
 
 class Tree
 
 	def initialize
+		@root = nil
+		@array = []
 	end
 
 	def build_tree(array)
-		array.each do |data|
-			node = Node.new(data)
-			
+		array = array.shuffle
+		array.each_with_index do |data, index|
+			if index == 0
+				@root = Node.new(data)
+				@array.push(@root) #for to_s
+			else
+				insert(@root,data,"", @root)
+			end
 		end
 	end
+
+	def insert(node, value, pos, parent)
+		if node == nil
+			child_node = Node.new(value)
+			@array.push(child_node)
+			if pos == "left"
+				parent.left_child = child_node 
+			else
+				parent.right_child = child_node
+			end
+			child_node.parent = node
+		elsif node.value <= value
+			insert(node.right_child, value, "right", node)
+		elsif node.value > value
+			insert(node.left_child, value, "left",node)
+		end
+	end
+
+	def to_s
+		string = ""
+		@array.each do |node|
+			string += node.value.to_s
+			right = node.right_child.value if node.right_child != nil
+			left = node.left_child.value if node.left_child != nil
+			string += " left: #{left}, right: #{right}\n"
+		end
+		return string
+	end
+
 end
+
+tree = Tree.new
+
+tree.build_tree([1,2,3,4,5,6,7,8,9])
+puts tree.to_s
